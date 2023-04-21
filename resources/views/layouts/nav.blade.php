f7c195940c4dc7d61530317236c0ee9a2886d391
Rousseau <maxime.rousseau99@gmail.com>
2023-04-21 16:24:41 +0200

    ajout de la barre de navigation et des imports dans le layout

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,97 @@
+@vite(['resources/css/nav.css'])
+
+<header>
+    <!-- Header Start -->
+    <div class="header-area">
+        <div class="main-header ">
+            <div class="header-top d-none d-lg-block">
+                <div class="container">
+                    <div class="col-xl-12">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="header-info-left">
+                                <ul>
+                                    <li>Numéro: 06 35 19 27 78</li>
+                                    <li>Email: stephane.pau@smartmoov.solutions</li>
+                                </ul>
+                            </div>
+                            {{--                            <div class="header-info-right">--}}
+                            {{--                                <ul class="header-social">--}}
+                            {{--                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>--}}
+                            {{--                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>--}}
+                            {{--                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>--}}
+                            {{--                                </ul>--}}
+                            {{--                            </div>--}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom  header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <!-- Logo -->
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo">
+                                <a href="#"><img src="{{ asset('img/nav/logo.png') }}" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10">
+                            <div class="menu-wrapper  d-flex align-items-center justify-content-end">
+                                <!-- Main-menu -->
+                                <div class="main-menu d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li>
+                                                <a href="{{ route('dashboard') }}"
+                                                   class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                                                    {{ __('Dashboard') }}
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="{{ route('profile.edit') }}">
+                                                    {{ __('Profile') }}
+                                                </a>
+                                            </li>
+
+                                            <li>
+                                                <a href="blog.html">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="blog_details.html">Blog Details</a></li>
+                                                    <li><a href="elements.html">Element</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="{{ route('offers') }}">
+                                                    {{ __('Offres') }}
+                                                </a></li>
+                                            <li>
+                                                <a href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">
+                                                    {{ __('Se déconnecter') }}
+                                                </a>
+                                                <form id="logout-form" method="POST" action="{{ route('logout') }}"
+                                                      style="display: none;">
+                                                    @csrf
+                                                </form>
+                                            </li>
+                                        </ul>
+
+                                    </nav>
+                                </div>
+                                <!-- Header-btn -->
+                                <div class="header-right-btn d-none d-lg-block ml-20">
+                                    <a href="contact.html" class="btn header-btn">Créer une session</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+</header>
