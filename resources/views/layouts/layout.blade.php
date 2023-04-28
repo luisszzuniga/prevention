d980ed568c4a541638b1f384b561dc0acb689000
Maxime22440 <maxime.rousseau99@gmail.com>
2023-04-28 14:06:15 +0200

    modification global du style pour home offers et nav

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -36,8 +36,9 @@
 @yield('content')
 
 
-
+    @include('layouts.back-top')
     @include('layouts.footer')
+
 </body>
 
 </html>
