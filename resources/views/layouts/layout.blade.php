f486152e0a74712b30f1859e146841476bec15c6
Rousseau <maxime.rousseau99@gmail.com>
2023-05-04 10:01:08 +0200

    résolution page offers

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,38 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/logo-lery.png') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    @vite(['resources/js/layout/vendor/modernizr-3.5.0.min.js'])
-    @vite(['resources/js/layout/vendor/jquery-1.12.4.min.js'])
-
-    @vite(['resources/js/layout/jquery.slicknav.min.js'])
-    @vite(['resources/js/layout/slick.min.js'])
-    @vite(['resources/js/layout/animated.headline.js'])
-    @vite(['resources/js/layout/jquery.sticky.js'])
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/jquery.slicknav.min.js"></script>
 
     @vite(['resources/js/layout/main.js'])
     @vite(['resources/js/app.ts'])
 
     @vite(['resources/css/style.css'])
-    @vite(['resources/css/layout/bootstrap.min.css'])
     @vite(['resources/css/layout/slicknav.css'])
-    @vite(['resources/css/layout/animate.min.css'])
     @vite(['resources/css/layout/slick.css'])
     @vite(['resources/css/style.css'])
     @vite(['resources/css/app.css'])
 
     <title> @yield('title','Base')</title>
     @yield('header')
-
-
-
 </head>
 
 <body>
     @include('layouts.nav')
 
 @yield('content')
-
 
     @include('layouts.back-top')
     @include('layouts.footer')
